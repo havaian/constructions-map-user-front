17ebf482f26e60b284f28e7f3db3cf17cc34da4f
Malik <m.usman.work@gmail.com>
2022-11-19 12:13:59 +0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -53,45 +53,41 @@
       };
     },
     mounted() {
-      if (document.cookie.split('=')[0] != 'approved') {
-          this.$router.push("/sign-in");
-          $('.menu-icon').css('display', 'none');
-      } else {
-        const app = (() => {
-          let body;
-          let menu;
-          let menuItems;
-          
-          const init = () => {
-            body = document.querySelector('body');
-            menu = document.querySelector('.menu-icon');
-            menuItems = document.querySelectorAll('.nav__list-item');
 
-            applyListeners();
-          }
-          
-          const applyListeners = () => {
-            menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
-          }
-          
-          const toggleClass = (element, stringClass) => {
-            if(element.classList.contains(stringClass))
-              element.classList.remove(stringClass);
-            else 
-              element.classList.add(stringClass);
-          }
-          
-          init();
-
-        })();
+      const app = (() => {
+        let body;
+        let menu;
+        let menuItems;
         
-        const menuItems = document.querySelectorAll('.nav__list-item');
+        const init = () => {
+          body = document.querySelector('body');
+          menu = document.querySelector('.menu-icon');
+          menuItems = document.querySelectorAll('.nav__list-item');
 
-        for (let x = 0; x < menuItems.length; x++) {
-          menuItems[x].addEventListener('click', () => {
-            document.querySelector('.menu-icon').click();
-          });
+          applyListeners();
         }
+        
+        const applyListeners = () => {
+          menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+        }
+        
+        const toggleClass = (element, stringClass) => {
+          if(element.classList.contains(stringClass))
+            element.classList.remove(stringClass);
+          else 
+            element.classList.add(stringClass);
+        }
+        
+        init();
+
+      })();
+      
+      const menuItems = document.querySelectorAll('.nav__list-item');
+
+      for (let x = 0; x < menuItems.length; x++) {
+        menuItems[x].addEventListener('click', () => {
+          document.querySelector('.menu-icon').click();
+        });
       }
     }
   }
